ebf4bea73fe53cc1a0af6ece1545998512e2e78d
fayssal <fayssal@example.com>
2026-02-25 00:54:05 +0100

    fixing misconceptions + kill-switch implementation, need to add some comments to explain intentions clearly but looks good

--- vpn.sh
+++ vpn.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+sudo iptables -t filter -D FORWARD -i wg0-test -s 10.0.0.2/32 -j ACCEPT 2>/dev/null || true
+sudo iptables -t filter -D FORWARD -o wg0-test -d 10.0.0.2/32 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 2>/dev/null || true
+sudo iptables -t nat -D POSTROUTING -o wlp3s0 -s 10.0.0.2/32 -j MASQUERADE 2>/dev/null || true
 sudo ip netns del wg-client 2>/dev/null || true
 sudo ip link del wg0-test 2>/dev/null || true
 
@@ -17,8 +20,6 @@ sudo ip -n wg-client link set veth0-wg-client name veth0
 sudo ip -n wg-client addr add 192.168.200.2/24 dev veth0
 sudo ip -n wg-client link set veth0 up
 
-sudo ip -n wg-client route add default via 192.168.200.1
-
 sudo ip link add wg0-test type wireguard
 sudo ip addr add 10.0.0.1/24 dev wg0-test
 wg genkey | tee /tmp/private.host | wg pubkey | tee /tmp/public.host
@@ -30,6 +31,14 @@ wg genkey | tee /tmp/private.ns | wg pubkey | tee /tmp/public.ns
 sudo ip -n wg-client link set wg0 up
 
 sudo wg set wg0-test private-key /tmp/private.host listen-port 51820 peer $(cat /tmp/public.ns) allowed-ips 10.0.0.2/32
-sudo ip netns exec wg-client wg set wg0 private-key /tmp/private.ns peer $(cat /tmp/public.host) endpoint 192.168.200.1:51820 allowed-ips 10.0.0.1/32
+sudo ip netns exec wg-client wg set wg0 private-key /tmp/private.ns peer $(cat /tmp/public.host) endpoint 192.168.200.1:51820 allowed-ips 0.0.0.0/0
 
-sudo ip -n wg-client route replace default via 10.0.0.1
+sudo ip -n wg-client route replace default dev wg0
+
+sudo iptables -t filter -P FORWARD DROP
+sudo iptables -t filter -A FORWARD -i wg0-test -s 10.0.0.2/32 -j ACCEPT
+sudo iptables -t filter -A FORWARD -o wg0-test -d 10.0.0.2/32 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+sudo iptables -t nat -A POSTROUTING -o wlp3s0 -s 10.0.0.2/32 -j MASQUERADE
+
+sudo sysctl -w net.ipv4.ip_forward=1
